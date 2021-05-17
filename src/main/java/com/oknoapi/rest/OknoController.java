569e428882b758a7876316896cbabca62c9cb807
LukpoDevs <71985109+LukpoDevs@users.noreply.github.com>
2021-05-17 22:41:07 +0200

    Add Fibonacci service

--- src/main/java/com/oknoapi/rest/OknoController.java
+++ src/main/java/com/oknoapi/rest/OknoController.java
@@ -2,25 +2,29 @@ package com.oknoapi.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oknoapi.rest.host.HostService;
+import com.oknoapi.rest.host.fibonacci.FibonacciService;
 
 @RestController
 public class OknoController {
 
 	@Autowired
-	private HostService service;
-
+	private HostService hostService;
+	
 	@GetMapping(path = "/")
 	public String imUpAndRunning() {
-		return "{healthy:true}";
+		return "Przetestuj API: <strong>localhost:8080/okno-api/n</strong> ,gdzie n to n-ty wyraz ciągu Fibonacciego";
 	}
 
-	
-	@GetMapping(path = "/okno-api")
-	public String helloWorld() {
-		return "Host aplikacji Spring: " + service.retrieveInstanceInfo();
+	@GetMapping(path = "/okno-api/{n}")
+	@ResponseBody
+	public String helloWorld(@PathVariable int n) {			
+		return "Host aplikacji Spring: " + hostService.retrieveInstanceInfo() + "<br>" +
+				"N-ta liczba Fibonacciego: " + FibonacciService.calculateFibonacci(n);
 	}
 
 	@GetMapping(path = "/okno-api-bean")
---
+++ src/main/java/com/oknoapi/rest/host/fibonacci/FibonacciService.java
@@ -0,0 +1,21 @@
+package com.oknoapi.rest.host.fibonacci;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FibonacciService {
+	
+    public static int calculateFibonacci(int n)
+    {
+        int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
